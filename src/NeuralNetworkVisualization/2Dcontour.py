ca924743732d69c0d3c8b1b003366fe8614401a4
fseldow <lixinhe126@126.com>
2018-03-07 14:38:12 -0500

    2D contour work for simple linear

--- src/NeuralNetworkVisualization/2Dcontour.py
+++ src/NeuralNetworkVisualization/2Dcontour.py
@@ -17,10 +17,28 @@ N=20000 #number of points in the train set
 mean = np.zeros(dim)
 cov = np.identity(dim)
 
-Map=np.zeros((N,2))
+#Map=np.zeros((N,2))
 
 [d1,d2,d] = np.random.multivariate_normal(mean, cov, 3)
 
+x = range(-100,100)
+y = range(-100,100)
+Map = []
+fnn=[]
+for i in x:
+    for j in y:
+        (A, y_single) = bd.readA_y(dim)
+        Y = np.ones((dim, 1)) * y_single
+        Map.append([i,j])
+        X_temp = min_pos + i*d1 + j*d2
+        Temp = Y - np.dot(A, X_temp)
+        fnn_temp = np.transpose(np.linalg.norm(Temp)**2)
+
+        fnn.append(fnn_temp)
+Map = np.asarray(Map)
+    #X_train = np.squeeze(np.transpose(Temp))
+
+"""
 for i in range(1,N):
     A = np.array([d1, d2, d])
     A2 = np.transpose(A)
@@ -31,10 +49,10 @@ for i in range(1,N):
     ret = np.dot(AA_inv, b)
     Map[i,0]=ret[0,0]
     Map[i,1]=ret[1,0]
-
+"""
 fig = plt.figure(1)
 ax = Axes3D(fig)
 surf = ax.plot_trisurf(np.asarray(Map[:,0]),np.asarray(Map[:,1]),np.squeeze(fnn),cmap='coolwarm',linewidth=0.2, antialiased=False)
-#plt.colorbar(surf)
+plt.colorbar(surf)
 plt.show()
 
